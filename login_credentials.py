6131476d8af52e8ad7ed716679ad12c004da6069
Kas <37886456+Kas-Akari@users.noreply.github.com>
2026-02-13 18:32:19 +0100

    Create login_credentials.py for user credentials
    Add an example file to store user login credentials for the bot.

---
+++ login_credentials.py
@@ -0,0 +1,17 @@
+#Archivo para almacenar las credenciales de login de los usuarios que controla el bot.
+#Recuerda añadir este archivo al .gitignore si quieres evitar la sincronización
+
+LOGIN_CREDENTIALS = {
+    'pololo': {    #Ejemplo de datos de login de usuario
+        'uni': '0',
+        'login': 'pololo@pitaya.es',
+        'pass': 'rambo',
+        'kid': ''
+    },
+    #'otro_usuario': {
+    #     'uni': '0',
+    #     'login': 'usuario@dominio.com',
+    #     'pass': 'contraseña',
+    #     'kid': '',
+    # },
+}
